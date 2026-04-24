72a412cb372f9fbe672d67aadb58937b9b7540f0
ruskerdax <ruskerdax@gmail.com>
2026-04-24 15:57:43 +0900

    decreased weapon range; increase railgun velocity

--- data/weapons.js
+++ data/weapons.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const WEAPONS=[
-  {id:'mass driver', dmg:3, cd:0.5, spd:7,    life:120},
-  {id:'railgun', dmg:2, cd:1.0, spd:10.5, life:180},
+  {id:'mass driver', dmg:3, cd:0.5, spd:7,    life:60},
+  {id:'railgun', dmg:2, cd:1.0, spd:12, life:90},
 ];
